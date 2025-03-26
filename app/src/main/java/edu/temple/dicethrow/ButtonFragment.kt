e50093d61ccda40e9a364de6f30e558c02a6ef93
Karl Morris <karl.morris@temple.edu>
2025-03-26 12:22:36 -0400

    Fix MainActivity compilation errors and fragment logic for portrait/landscape

--- app/src/main/java/edu/temple/dicethrow/ButtonFragment.kt
+++ app/src/main/java/edu/temple/dicethrow/ButtonFragment.kt
@@ -1,39 +1,45 @@
-package edu.temple.dicethrow
+    package edu.temple.dicethrow
 
-import android.content.Context
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Button
-import androidx.lifecycle.ViewModelProvider
+    import android.content.Context
+    import android.os.Bundle
+    import androidx.fragment.app.Fragment
+    import android.view.LayoutInflater
+    import android.view.View
+    import android.view.ViewGroup
+    import android.widget.Button
+    import androidx.lifecycle.ViewModelProvider
 
-class ButtonFragment : Fragment() {
+    class ButtonFragment : Fragment() {
 
-    private val dieViewModel: DieViewModel by lazy {
-        ViewModelProvider(requireActivity())[DieViewModel::class.java]
-    }
+        private val dieViewModel: DieViewModel by lazy {
+            ViewModelProvider(requireActivity())[DieViewModel::class.java]
+        }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_button, container, false).apply {
-            findViewById<Button>(R.id.rollButton).setOnClickListener {
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            return inflater.inflate(R.layout.fragment_button, container, false).apply {
+                findViewById<Button>(R.id.rollButton).setOnClickListener {
 
-                // Generate a random die roll
-                dieViewModel.rollDie()
+                    // Generate a random die roll
+                    dieViewModel.rollDie()
 
-                // Notify activity
-                (requireActivity() as ButtonInterface).buttonClicked()
+                    // Notify activity via interface that it implements
+                    (requireActivity() as ButtonInterface).buttonClicked()
+                }
             }
         }
+
+        /**
+         * Interface to be realized by parent activity
+         * to allow communication without ViewModel
+         * This allows an event occurrence to be propagated even
+         * with no associated state change
+         */
+        interface ButtonInterface {
+            fun buttonClicked()
+        }
+
+
     }
-
-    interface ButtonInterface {
-        fun buttonClicked()
-    }
-
-
-}
--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -1,11 +1,8 @@
 package edu.temple.dicethrow
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
+import androidx.lifecycle.ViewModelProvider
 
 /*
 Our DieThrow application has been refactored to move the dieRoll() logic
@@ -19,22 +16,56 @@ The Activity layout files for both Portrait and Landscape are already provided
 */
 
 class MainActivity : AppCompatActivity(), ButtonFragment.ButtonInterface {
+
+    private var hasTwoColumns = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         /* TODO 1: Load fragment(s)
-            - Show only Button Fragment if portrait
-            - show both fragments if Landscape
-          */
+            - Show _only_ ButtonFragment if portrait
+            - show _both_ fragments if Landscape
+        */
+
+        val dieViewModel = ViewModelProvider(this)[DieViewModel::class.java]
+
+        hasTwoColumns = findViewById<android.view.View>(R.id.container2) != null
+
+        val buttonFragment = ButtonFragment()
+        val dieFragment = DieFragment()
+
+        if (savedInstanceState == null) {
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.add(R.id.container1, buttonFragment)
+            transaction.commit()
+
+            if (hasTwoColumns) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container1, buttonFragment)
+                    .add(R.id.container2, dieFragment)
+                    .commit()
+            }
+        }
+
+        // Observe die roll LiveData and switch fragments if in portrait
+        dieViewModel.getDieRoll().observe(this) {
+            if (!hasTwoColumns) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container1, dieFragment)
+                    .setReorderingAllowed(true)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
     }
 
-    /* TODO 2: switch fragments if portrait (no need to switch fragments if Landscape)
-        */
+    /* TODO 2: switch fragments if die rolled and in portrait (no need to switch fragments if Landscape)
+    */
+
+    // This callback function gets invoked when the child Fragment invokes it
     // Remember to place Fragment transactions on BackStack so then can be reversed
     override fun buttonClicked() {
-
+        // Implement behavior for button click if needed
     }
-
-
 }
